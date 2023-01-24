9016d6644156f52522c761721430bb190ad9e234
corno <corno.astn.a@schraverus.com>
2023-01-24 23:43:02 +0100

    .

--- pub/src/modules/public/implementation.generated.ts
+++ pub/src/modules/public/implementation.generated.ts
@@ -1,6 +1,6 @@
 import { API } from "./api"
-import { $$ as idiffData } from "./implementations/diffData.p"
-import { $$ as istringsAreEqual } from "./implementations/stringsAreEqual.p"
+import { $$ as idiffData } from "./implementations/diffData.native"
+import { $$ as istringsAreEqual } from "./implementations/stringsAreEqual.native"
 
 export const $a: API = {
     'diffData': idiffData,
