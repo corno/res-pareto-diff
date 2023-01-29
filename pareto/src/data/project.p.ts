00cf4bc9c5f7c06f8d4455bc5816bf70a3e85158
corno <corno.astn.a@schraverus.com>
2023-01-30 00:57:19 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -7,11 +7,26 @@ import { $ as api } from "./api.p"
 const d = pr.wrapRawDictionary
 
 export const $: mproject.TProject = {
-    'type': ['resource', {}],
+    'name': "res-pareto-diff",
+
+    'author': "Corno",
+    'description': "a pareto wrapper for the diff library 'http://github.com/kpdecker/jsdiff'",
+    'license': "ISC",
+
+    'type': ['resource',  {
+        'devDependencies': d({
+            "@types/diff": {},
+        }),
+    }],
     'modules': d({
         "public": {
             'definition': api,
         },
     }),
-    'main': "public"
+    'main': "public",
+    'pubdependencies': d({
+        "glo-pareto-common": {},
+        "diff": {},
+    }),
+    'testdependencies': d({}),
 }
