581fba4321696a42317fb395adcde90b4843f68f
corno <corno.astn.a@schraverus.com>
2023-02-04 09:29:00 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -2,7 +2,7 @@ import * as pr from 'pareto-core-raw'
 
 import * as mproject from "lib-pareto-typescript-project/dist/submodules/project"
 
-import { $ as api } from "./api.p"
+import { $ as api } from "./api.data"
 
 const d = pr.wrapRawDictionary
 
