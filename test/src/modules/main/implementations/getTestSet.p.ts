30a8a022e64879c57eda095ee733da985b7eb4b6
corno <corno.astn.a@schraverus.com>
2023-02-12 09:08:25 +0100

    .

--- test/src/modules/main/implementations/getTestSet.p.ts
+++ test/src/modules/main/implementations/getTestSet.p.ts
@@ -11,7 +11,7 @@ import * as pub from "../../../../../pub"
 
 export const $$: api.CgetTestSet = () => {
 
-    const builder = ps.createUnsafeDictionaryBuilder<mtest.TTestElement>()
+    const builder = ps.createUnsafeDictionaryBuilder<mtest.T.TestElement>()
     function createTest(name: string, actual: string, expected: string) {
         builder.add(name, {
             type: ['test', {
