cbabf2abf512285e477a17614f25ead4b08a826c
corno <corno.astn.a@schraverus.com>
2023-04-11 15:38:05 +0200

    .

--- typescript/pub/src/implementation.generated.ts
+++ typescript/pub/src/implementation.generated.ts
@@ -2,7 +2,7 @@ import { API } from "./api.generated"
 import { $$ as idiffData } from "./implementations/diffData.native"
 import { $$ as istringsAreEqual } from "./implementations/stringsAreEqual.native"
 
-export const $r: API = {
+export const $api: API = {
     'diffData': idiffData,
     'stringsAreEqual': istringsAreEqual,
 }
